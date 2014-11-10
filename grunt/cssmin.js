31d12a33b7ede51976034b6b9c4fe9b25e9b98fd
Jason Storey <jason.andrew.storey@gmail.com>
2014-11-10 00:23:44 +0000

    Add placeholder header section

--- grunt/cssmin.js
+++ grunt/cssmin.js
@@ -2,7 +2,7 @@ module.exports = function(grunt, options) {
     return {
         combine: {
             files: {
-                'dist/styles.css': ['src/css/reset.css']
+                'dist/styles.css': ['src/css/reset.css', 'src/css/general.css', 'src/css/header.css']
             }
         }
     };
